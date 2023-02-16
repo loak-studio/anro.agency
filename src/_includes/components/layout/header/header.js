882236b3d34e0e23afd923464c9c4a65c063d6f0
Benjamin Geets <bge@tuta.io>
2023-02-16 12:55:58 +0100

    fix(header): create div to close dropdown instead of pseudo element to prevent opacity bug

--- src/_includes/components/layout/header/header.js
+++ src/_includes/components/layout/header/header.js
@@ -1,11 +1,17 @@
 const subLists = document.querySelectorAll('button.header__navigation-label')
 const header = document.querySelector('.header')
+const body = document.querySelector('body')
+const closeLayout = document.createElement('div')
+closeLayout.classList.add('close-layout')
+
+
 subLists.forEach(subList=>{
   subList.addEventListener('click',()=>{
     subList.nextElementSibling.classList.add('header__sub-navigation--active')
-    const subNavigation = document.querySelector('.header__sub-navigation--active')
-    subNavigation.addEventListener('click',()=>{
+    body.appendChild(closeLayout)
+    closeLayout.addEventListener('click',()=>{
       subList.nextElementSibling.classList.remove('header__sub-navigation--active')
+      body.removeChild(closeLayout)
     }, {once:true})
   })
 })
