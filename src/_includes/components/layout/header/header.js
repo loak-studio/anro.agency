751beadb24bbef2eb9f7c8cd32a875764c4cc0db
Benjamin Geets <bge@tuta.io>
2023-02-16 11:59:30 +0100

    feat(header): social links & hamburger

--- src/_includes/components/layout/header/header.js
+++ src/_includes/components/layout/header/header.js
@@ -1,7 +1,17 @@
 const subLists = document.querySelectorAll('button.header__navigation-label')
-
+const header = document.querySelector('.header')
 subLists.forEach(subList=>{
   subList.addEventListener('click',()=>{
-    subList.nextElementSibling.classList.toggle('active')
+    subList.nextElementSibling.classList.add('header__sub-navigation--active')
+    const subNavigation = document.querySelector('.header__sub-navigation--active')
+    subNavigation.addEventListener('click',()=>{
+      subList.nextElementSibling.classList.remove('header__sub-navigation--active')
+    }, {once:true})
   })
+})
+
+const button = document.querySelector('#hamburger')
+
+button.addEventListener('click',()=>{
+  header.classList.toggle('header--active')
 })
