9e1b2d7cc87bba434679f2ef9233b619530b8ccc
Benjamin Geets <bge@tuta.io>
2023-02-20 15:11:43 +0100

    feat: perf optimziation (remove swiper from index, add seo title, ...)

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,2 +1,1 @@
 import "../../src/_includes/components/layout/header/header.js"
-import "../../src/_includes/components/portfolio/portfolio-list/portfolio-list.js"
---
+++ public/scripts/swiper.js
@@ -0,0 +1,1 @@
+import "../../src/_includes/components/portfolio/portfolio-list/portfolio-list.js"
