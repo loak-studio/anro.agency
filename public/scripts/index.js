2a9f59212fb37d69f97b2cef677c6e5a912cd8ac
Benjamin Geets <bge@tuta.io>
2023-02-20 11:14:10 +0100

    feat(portfolio): slides & titles

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,1 +1,2 @@
 import "../../src/_includes/components/layout/header/header.js"
+import "../../src/_includes/components/portfolio/portfolio-list/portfolio-list.js"
---
+++ src/_includes/components/portfolio/portfolio-list/portfolio-list.js
@@ -0,0 +1,36 @@
+import Swiper from 'swiper';
+import 'swiper/css';
+
+const sliders = document.querySelectorAll('.portoflio-list__slider')
+
+
+sliders.forEach(slider=>{
+  const previous = slider.querySelector('.previous')
+  const next = slider.querySelector('.next')
+  const swiperContainer = slider.querySelector('.swiper')
+  let swiper = new Swiper(swiperContainer, {
+    loop:true,
+    slidesPerView: 1.1,
+
+    spaceBetween:20,
+    breakpoints: {
+      1200: {
+        slidesPerView: 3,
+      },
+      900: {
+        slidesPerView: 2,
+        spaceBetween:60,
+      }
+   }
+  });
+
+  previous.addEventListener('click',()=>{
+    swiper.slidePrev()
+  })
+  next.addEventListener('click',()=>{
+    swiper.slideNext()
+  })
+})
+
+
+
