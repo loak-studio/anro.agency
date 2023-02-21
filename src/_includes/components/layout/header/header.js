bfde63f231466da7aaede48df992055f957481af
Benjamin Geets <bge@tuta.io>
2023-02-21 16:30:02 +0100

    feat(header): quit sublist with escape key

--- src/_includes/components/layout/header/header.js
+++ src/_includes/components/layout/header/header.js
@@ -5,21 +5,35 @@ const closeLayout = document.createElement('div')
 closeLayout.classList.add('close-layout')
 
 
-subLists.forEach(subList=>{
-  subList.addEventListener('click',()=>{
-    subList.nextElementSibling.classList.add('header__sub-navigation--active')
-    body.appendChild(closeLayout)
-    closeLayout.addEventListener('click',()=>{
+subLists.forEach(subList => {
+
+
+  const clickListener = (e) => {
+    subList.nextElementSibling.classList.remove('header__sub-navigation--active')
+    body.removeChild(closeLayout)
+    document.removeEventListener('keydown', keyListener)
+  }
+
+  const keyListener = (e) => {
+    if (e.key == "Escape") {
       subList.nextElementSibling.classList.remove('header__sub-navigation--active')
       body.removeChild(closeLayout)
-    }, {once:true})
+      closeLayout.removeEventListener('click', clickListener)
+    }
+  }
+
+  subList.addEventListener('click', () => {
+    subList.nextElementSibling.classList.add('header__sub-navigation--active')
+    body.appendChild(closeLayout)
+    document.addEventListener('keydown', keyListener, { once: true })
+    closeLayout.addEventListener('click', clickListener, { once: true })
   })
 })
 
 const button = document.querySelector('#hamburger')
 
-button.addEventListener('click',()=>{
-  window.scrollTo(0,0)
+button.addEventListener('click', () => {
+  window.scrollTo(0, 0)
   document.querySelector('html').classList.toggle('nav-open')
   header.classList.toggle('header--active')
 })
