85c4e965ced1d387bb08e31d26d9725e4805e99d
Benjamin Geets <bge@tuta.io>
2023-02-21 11:30:41 +0100

    feat(portfolio): project header & review

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,14 @@ const EleventyVitePlugin = require("@11ty/eleventy-plugin-vite");
 module.exports = function(eleventyConfig) {
 
   eleventyConfig.addFilter("log", function(value) { console.log(" --- LOG:",value) });
-
+  eleventyConfig.addFilter("year", (value)=>{
+    const date = new Date(value)
+    return date.getFullYear()
+  })
+  eleventyConfig.addFilter("monthAndYear", (value)=>{
+    const date = new Date(value)
+    return date.toLocaleDateString('fr-fr', {month:'long', year:'numeric'})
+  })
   eleventyConfig.addPassthroughCopy({ "public": "/" })
   eleventyConfig.addPlugin(EleventyVitePlugin);
   
