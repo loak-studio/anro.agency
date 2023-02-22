45d2bb11db3fa46fb163d6378fe04adbc0d379dc
Benjamin Geets <bge@tuta.io>
2023-02-22 15:56:36 +0100

    feat: use img shortcode

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 const EleventyVitePlugin = require("@11ty/eleventy-plugin-vite");
+require('dotenv').config()
 module.exports = function(eleventyConfig) {
 
   eleventyConfig.addFilter("log", function(value) { console.log(" --- LOG:",value) });
@@ -6,12 +7,18 @@ module.exports = function(eleventyConfig) {
     const date = new Date(value)
     return date.getFullYear()
   })
+
   eleventyConfig.addFilter("monthAndYear", (value)=>{
     const date = new Date(value)
     return date.toLocaleDateString('fr-fr', {month:'long', year:'numeric'})
   })
-  eleventyConfig.addPlugin(EleventyVitePlugin);
+  eleventyConfig.addShortcode('img', (image, width, height)=>{
+    const url = process.env.API + "/assets/" + image + "?quality=50&fit=cover" + (width? '&width=' + width : '') + (height? '&height=' + height : '')
+    return url
   
+  })
+
+  eleventyConfig.addPlugin(EleventyVitePlugin);
   return {
     dir:{
       input:'src'
