5acf2dfae90bb84e80d0a1ced88147835f6ec09f
Benjamin Geets <bge@tuta.io>
2023-02-22 15:58:40 +0100

    fix(hero): image size

--- .eleventy.js
+++ .eleventy.js
@@ -1,27 +1,29 @@
 const EleventyVitePlugin = require("@11ty/eleventy-plugin-vite");
 require('dotenv').config()
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
 
-  eleventyConfig.addFilter("log", function(value) { console.log(" --- LOG:",value) });
-  eleventyConfig.addFilter("year", (value)=>{
+  eleventyConfig.addFilter("log", function (value) { console.log(" --- LOG:", value) });
+
+  eleventyConfig.addFilter("year", (value) => {
     const date = new Date(value)
     return date.getFullYear()
   })
 
-  eleventyConfig.addFilter("monthAndYear", (value)=>{
+  eleventyConfig.addFilter("monthAndYear", (value) => {
     const date = new Date(value)
-    return date.toLocaleDateString('fr-fr', {month:'long', year:'numeric'})
+    return date.toLocaleDateString('fr-fr', { month: 'long', year: 'numeric' })
   })
-  eleventyConfig.addShortcode('img', (image, width, height)=>{
-    const url = process.env.API + "/assets/" + image + "?quality=50&fit=cover" + (width? '&width=' + width : '') + (height? '&height=' + height : '')
+
+  eleventyConfig.addShortcode('img', (image, width, height) => {
+    const url = process.env.API + "/assets/" + image + "?quality=50&fit=cover" + (width ? '&width=' + width : '') + (height ? '&height=' + height : '')
     return url
-  
+
   })
 
   eleventyConfig.addPlugin(EleventyVitePlugin);
   return {
-    dir:{
-      input:'src'
+    dir: {
+      input: 'src'
     }
   }
 };
